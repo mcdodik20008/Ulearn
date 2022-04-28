e2e9862e6672da113fcda57c7611c40f9e05df71
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2022-04-28 15:47:20 +0500

    Долго работает

---
+++ BinaryTrees/BinaryTree.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinaryTrees
+{
+    public class BinaryTree<T> : IEnumerable<T>
+        where T : IComparable
+    {
+        BinaryNode<T> rootNode = null;
+        public void Add(T e)
+        {
+            if (rootNode == null)
+                rootNode = new BinaryNode<T>(e, 0);
+            else
+                Add(rootNode, e, 1);
+        }
+
+        public void Add(BinaryNode<T> parent, T e, int depth)
+        {
+            bool isRight = e.CompareTo(parent.Value) >= 0;
+            if (isRight && parent.Right == null)
+                parent.Right = new BinaryNode<T>(e, depth);
+            else if (!isRight && parent.Left == null)
+                parent.Left = new BinaryNode<T>(e, depth);
+            else
+                Add(isRight ? parent.Right : parent.Left, e, depth+1);
+        }
+
+        public bool Contains(T item) =>
+            rootNode != null && Search(rootNode, item) != null;
+        
+
+        public static BinaryNode<T> Search(BinaryNode<T> root, T element)
+        {
+            if (root == null) return null;
+            if (element.Equals(root.Value)) return root;
+            return element.CompareTo(root.Value) >= 0
+                ? Search(root.Right, element)
+                : Search(root.Left, element);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            if (rootNode != null)
+                foreach (var item in rootNode)
+                    yield return item;
+                
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            if (rootNode != null)
+                foreach (var item in rootNode)
+                    yield return item;
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                var currentNode = rootNode;
+                while (true)
+                {
+                    var leftSize = currentNode.Left?.Depth ?? 0;
+                    if (i == leftSize)
+                        return currentNode.Value;
+                    if (i < leftSize)
+                        currentNode = currentNode.Left;
+                    else
+                    {
+                        currentNode = currentNode.Right;
+                        i -= 1 + leftSize;
+                    }
+                }
+            }//доделать
+        }
+    }
+
+    public class BinaryNode<T> : IEnumerable<T>
+        where T : IComparable
+    {
+        public T Value;
+        public BinaryNode<T> Left, Right;
+        public int Depth;
+
+        public BinaryNode(T val, int depth)
+        {
+            Value = val;
+            Depth = depth;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            IEnumerable<T> collection = GetCollection(this);
+            foreach (var item in collection)
+                yield return item;  
+        }
+
+        private IEnumerable<T> GetCollection(BinaryNode<T> parent)
+        {
+            var list = new List<T>();
+            if (parent == null)
+                return new List<T>(0);
+            list.Add(parent.Left.Value);
+            list.AddRange(GetCollection(parent.Left));
+            list.Add(parent.Right.Value);
+            list.AddRange(GetCollection(parent.Right));
+            return list;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+             return GetEnumerator();
+        }
+    }
+}
---
+++ BinaryTrees/Program.cs
@@ -0,0 +1,80 @@
+using NUnit.Framework;
+using NUnitLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunWithTimeout(MixedTest, 500, "Your tree is too slow");
+        }
+
+        public static void RunWithTimeout(Action work, int timeout, string message)
+        {
+            var task = Task.Run(work);
+            if (!task.Wait(timeout))
+                throw new AssertionException(message);
+        }
+
+        public static void MixedTest()
+        {
+            var tree = new BinaryTree<int>();
+            var indexer = MakeAccessor(tree);
+            var type = typeof(BinaryTreeInTask2_should);
+            var stream = type.Assembly.GetManifestResourceStream("BinaryTrees.test.txt")
+                         ?? throw new Exception("no resource!");
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine() ?? throw new Exception("Command expected!");
+                    var values = line.Split();
+                    var command = values.First();
+                    if (command == "Add")
+                    {
+                        foreach (var value in values.Skip(1))
+                        {
+                            tree.Add(int.Parse(value));
+                        }
+                    }
+                    else
+                    {
+                        var index = int.Parse(values[1]);
+                        var value = int.Parse(values[2]);
+                        Assert.AreEqual(value, indexer(index));
+                    }
+                }
+            }
+        }
+
+        public static PropertyInfo GetIndexer<T>(BinaryTree<T> t)
+    where T : IComparable
+        {
+            return t.GetType()
+                .GetProperties()
+                .Select(z => new { prop = z, ind = z.GetIndexParameters() })
+                .SingleOrDefault(z => z.ind.Length == 1 && z.ind[0].ParameterType == typeof(int))
+                ?.prop;
+        }
+
+        public static Func<int, T> MakeAccessor<T>(BinaryTree<T> tree)
+            where T : IComparable
+        {
+            var prop = GetIndexer(tree);
+            var param = Expression.Parameter(typeof(int));
+            return Expression.Lambda<Func<int, T>>(
+                    Expression.MakeIndex(Expression.Constant(tree), prop, new[] { param }),
+                    param)
+                .Compile();
+        }
+    }
+}
